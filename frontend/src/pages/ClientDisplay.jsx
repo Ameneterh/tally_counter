cc67f7e1611eac7a46cb1196d505abec4a9cea2e
Amene Terhemen <ameneterh@gmail.com>
2024-11-12 18:43:45 +0100

    optimized client list display ui for mobile devices

--- frontend/src/pages/ClientDisplay.jsx
+++ frontend/src/pages/ClientDisplay.jsx
@@ -31,11 +31,11 @@ export default function ClientDisplay() {
   }, [totalClients]);
 
   return (
-    <div className="min-h-[80svh] flex flex-col gap-6 max-w-6xl mx-auto mt-5 px-10">
+    <div className="min-h-[80svh] flex flex-col gap-6 w-full max-w-6xl mx-auto mt-5 px-10">
       <div className="text-3xl w-full font-bold text-center">
         Clients in Attendance:
       </div>
-      <div className="w-full grid grid-cols-3 gap-6">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-6">
         {clients.map((client, id) => (
           <ShowClient
             key={id}
