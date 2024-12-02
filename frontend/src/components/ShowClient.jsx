2a560dee8c363b6c092868642889e3671572a138
Amene Terhemen <ameneterh@gmail.com>
2024-12-02 12:12:48 +0100

    more adjustment to location text

--- frontend/src/components/ShowClient.jsx
+++ frontend/src/components/ShowClient.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function ShowClient({ tally, location }) {
   return (
-    <div className="shadow max-w-5xl p-10 bg-blue-950 text-white flex flex-col text-center rounded">
+    <div className="shadow max-w-4xl p-10 bg-blue-950 text-white flex flex-col text-center rounded">
       <div className="text-8xl font-extrabold">{tally}</div>
-      <div className="font-bold text-[45px]">{location}</div>
+      <div className="font-bold text-5xl">{location}</div>
     </div>
   );
 }
